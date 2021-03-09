6ac0792ebc978fd599c2f50884bae105a1d32e11
seagalputra <seagalputra@gmail.com>
2021-03-09 13:57:18 +0700

    Change migration schema in transaction detail table

--- db/migrations/000002_create_table_transaction_detail.up.sql
+++ db/migrations/000002_create_table_transaction_detail.up.sql
@@ -2,7 +2,9 @@ CREATE TYPE transaction_status AS ENUM ('income', 'outcome', 'waiting');
 CREATE TABLE IF NOT EXISTS transaction_detail (
 	id BIGSERIAL PRIMARY KEY,
 	transaction_detail_id VARCHAR(100),
-	amount NUMERIC(19,4),
+    needs NUMERIC(19,4),
+    wants NUMERIC(19,4),
+    invest NUMERIC(19,4),
 	description TEXT,
 	status transaction_status
 );
