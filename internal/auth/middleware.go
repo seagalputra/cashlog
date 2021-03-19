20fd53b710eb8fd8114835019f824258f9ce3201
seagalputra <seagalputra@gmail.com>
2021-03-19 08:02:34 +0700

    Adding middleware

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -3,5 +3,9 @@ package auth
 import "net/http"
 
 func Middleware() func(http.Handler) http.Handler {
-	return nil
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(rw, r)
+		})
+	}
 }
