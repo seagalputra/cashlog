8c1b5bab220debb00b8f57f4c9f9297e60741991
seagalputra <seagalputra@gmail.com>
2022-07-13 09:50:47 +0700

    re-route expense view

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,6 @@
 class Expense < ApplicationRecord
+  OUTCOME = 'outcome'.freeze
+  INCOME = 'income'.freeze
+
+  enum expense_type: [:income, :outcome]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :expenses
   # devise_for :users
-  root 'home#index'
+  resources :expenses
+
+  root 'expenses#index'
 end
